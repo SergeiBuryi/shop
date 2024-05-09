194340e4785c4a9450e184f31816b0908f3e33e3
Sergei <sergeiburyi1@gmail.com>
2024-05-10 01:01:07 +0300

    fix react deploy

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
-import { store } from './store/store';
-import { Provider } from 'react-redux';
+// import { store } from './store/store';
+// import { Provider } from 'react-redux';
+//
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App />,
+//     },
+// ]);
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-]);
-
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <Provider store={store}>
+//         <RouterProvider router={router} />
+//     </Provider>
+// );
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <RouterProvider router={router} />
-    </Provider>
+    <App />
 );
+
