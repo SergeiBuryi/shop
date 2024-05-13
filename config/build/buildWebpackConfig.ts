8bb8febced573df949e6987fefa2968e9a846892
Sergei <sergeiburyi1@gmail.com>
2024-05-13 19:49:51 +0300

    add webpack, typescript; setting webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import {BuildOptions} from "./types/config";
+import {WebpackConfiguration} from "webpack-cli";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+
+export function buildWebpackConfig(options: BuildOptions): WebpackConfiguration {
+    const {mode,paths} = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    };
+}
