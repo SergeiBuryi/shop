c69daa8f0d2b4e0b45a9ea173194c7be59c967e7
Sergei <sergeiburyi1@gmail.com>
2024-05-20 01:33:48 +0300

    add Architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
-import ThemeProvider from "./theme/ThemeProvider";
+import App from "./app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
