38ab89bf599b29bd1f944eb43d191e6f9269f53e
Sergei <sergeiburyi1@gmail.com>
2024-06-06 03:15:41 +0300

    Updates

---
+++ 529.7d13f6ae64819aafd6c1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[529],{529:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});var c=t(848);const r=function(){return(0,c.jsx)("div",{children:"Cart"})}}}]);
---
+++ 833.23d89b6cb763285870c1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[833],{833:(e,n,t)=>{t.r(n),t.d(n,{default:()=>s});var c=t(848);const s=function(){return(0,c.jsx)("div",{children:"Favorites"})}}}]);
